0e1aa8cff95804abbd881a192dc4cd1355c140c1
Anthony Ortiz <ortiz.anthony@gmail.com>
2020-05-10 10:14:33 -0400

    prep statement

--- src/main/java/com/pluralsight/order/dao/GetOrderDao.java
+++ src/main/java/com/pluralsight/order/dao/GetOrderDao.java
@@ -33,7 +33,7 @@ public class GetOrderDao {
     public OrderDto getOrderById(ParamsDto paramsDto) {
         OrderDto orderDto = null;
 
-        try (Connection con = null;
+        try (Connection con = database.getConnection();
              PreparedStatement ps = createPreparedStatement(con, paramsDto.getOrderId());
              ResultSet rs = createResultSet(ps)
         ) {
@@ -53,8 +53,9 @@ public class GetOrderDao {
      * @throws SQLException In case of an error
      */
     private PreparedStatement createPreparedStatement(Connection con, long orderId) throws SQLException {
-
-        return null;
+        PreparedStatement statement = con.prepareStatement(query);
+        statement.setLong(1, orderId);
+        return statement;
     }
 
     /**
